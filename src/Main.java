37a6f1c82875649a7fcb17cf55962077606a8933
Brakhotskaya Olga <brakhotskaya.oll@gmail.com>
2022-06-05 18:56:50 +0300

    First iteration

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1000;
+        int oneMilePrice = 20;
+        int bonusMiles = ticketPrice / oneMilePrice;
+        System.out.println(bonusMiles);
+    }
+}
